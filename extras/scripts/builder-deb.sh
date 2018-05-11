a2ce923e502ac11cdc2c02c8ac73f792c7303558
Patrick Wu J M <wotingwu@live.com>
2018-05-10 22:24:12 -0500

    first build for debian/ubuntu/kali linux completed

--- extras/scripts/builder-deb.sh
+++ extras/scripts/builder-deb.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-BUILD_VER=`grep 'Version: ' ../build/debian-build/pkg-debian/DEBIAN/control | cut -d':' -f 2 | xargs`
+BUILD_VER=`grep 'Version: ' ../../build/debian-build/DEBIAN/control | cut -d':' -f 2 | xargs`
 cp ../../src/wsl* ../../build/debian-build/usr/bin/
 cd ../../build/debian-build/
 find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
 
 BUILD_DIR=`mktemp --tmpdir --directory pkg-debian.XXXX`
 
-cp -rp * $BUILD_DIR/
+cp -rp * $BUILD_DIR
+
 find $BUILD_DIR -type d -exec chmod 0755 {} \;
 find $BUILD_DIR/usr/bin -type f -exec chmod 0555 {} \;
 
-cd ..
+cd ../debian-release
 
 dpkg -b $BUILD_DIR/ wslu-${BUILD_VER}.deb
 
