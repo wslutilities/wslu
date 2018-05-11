8c2e67f5b9a5351cc30810576cba495abb1304b0
Patrick Wu J M <wotingwu@live.com>
2018-05-10 23:08:42 -0500

    Better build without using thi git repository

--- extras/scripts/builder-deb.sh
+++ extras/scripts/builder-deb.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-mkdir ../../release/debian-build/
-mkdir ../../release/debian-build/DEBIAN/
-mkdir ../../release/debian-build/usr/
-mkdir ../../release/debian-build/usr/bin/
+BUILD_DIR=`mktemp --tmpdir --directory pkg-debian.XXXX`
+CURRENT_DIR=`pwd`
 
-cat <<EOF >>../../release/debian-build/DEBIAN/control
+mkdir $BUILD_DIR/DEBIAN/
+mkdir $BUILD_DIR/usr/
+mkdir $BUILD_DIR/usr/bin/
+
+cat <<EOF >>$BUILD_DIR/DEBIAN/control
 Package: wslu
 Architecture: all
 Maintainer: patrick330602 <wotingwu@live.com>
@@ -14,22 +16,17 @@ Version: 1.3
 Description: A collection of utilities for Windows 10 Linux Subsystem
  This is a collection of utilities for Windows 10 Linux Subsystem, such as enabling sound in WSL or creating your favorite linux app shortcuts on Windows 10 Desktop. Requires Windows 10 Creators Update and higher.
 EOF
-BUILD_VER=`grep 'Version: ' ../../release/debian-build/DEBIAN/control | cut -d':' -f 2 | xargs`
-cp ../../src/wsl* ../../release/debian-build/usr/bin/
+BUILD_VER=`grep 'Version: ' $BUILD_DIR/DEBIAN/control | cut -d':' -f 2 | xargs`
+cp ../../src/wsl* $BUILD_DIR/usr/bin/
 
-cd ../../release/debian-build/
+cd $BUILD_DIR
 find . -type f ! -regex '.*.hg.*' ! -regex '.*?debian-binary.*' ! -regex '.*?DEBIAN.*' -printf '%P ' | xargs md5sum > DEBIAN/md5sums
-
-BUILD_DIR=`mktemp --tmpdir --directory pkg-debian.XXXX`
-
-cp -rp * $BUILD_DIR
 
 find $BUILD_DIR -type d -exec chmod 0755 {} \;
 find $BUILD_DIR/usr/bin -type f -exec chmod 0555 {} \;
 
-cd ../debian
+cd $CURRENT_DIR/../../release/debian
 
 dpkg -b $BUILD_DIR/ wslu-${BUILD_VER}.deb
 
 rm -rf $BUILD_DIR
-rm -rf ../debian-build
