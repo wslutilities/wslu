bc653d6220449a876535da91f10dd3a872fd48bb
Patrick Wu J M <wotingwu@live.com>
2018-05-12 11:14:32 -0500

    update feature pending

--- extras/scripts/update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#this script will try to update from base release
-echo "Updating from the git repository..."
-git --git-dir=~/.wslu/.git/ --work-tree=~/.wslu/ fetch
-git --git-dir=~/.wslu/.git/ --work-tree=~/.wslu/ pull
-git --git-dir=~/.wslu/.git/ --work-tree=~/.wslu/ fetch --tags
-git --git-dir=~/.wslu/.git/ --work-tree=~/.wslu/ checkout $(git describe --tags $(git rev-list --tags --max-count=1))
