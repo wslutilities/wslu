5a7fbb23f1aae9376e0cd60443a6d6eb384d76c6
Patrick Wu <wotingwu@live.com>
2019-01-06 13:47:57 +0800

    project: incomplete header changes

--- extras/scripts/builder-docs.sh
+++ extras/scripts/builder-docs.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
+# builder-deb.sh
+# scripts of wslu
+# <https://github.com/wslutilities/wslu>
+# Copyright (C) 2019 Patrick Wu
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 git clone https://github.com/wslutilities/wslu.wiki.git
 
 OUTPATH="../../gendocs"
