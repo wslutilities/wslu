dfd7a44b2079918ebc713bb31eec9d28dd60563d
Balint Reczey <balint.reczey@canonical.com>
2019-07-09 20:36:11 +0200

    Use PulseAudio if it is running on Windows
    LP: #1836552

--- debian/wsl-integration.sh
+++ debian/wsl-integration.sh
@@ -3,3 +3,10 @@ if which xvinfo > /dev/null && env DISPLAY=:0 xvinfo > /dev/null 2>&1; then
     export DISPLAY=:0
     export LIBGL_ALWAYS_INDIRECT=1
 fi
+
+# set up audio if pulse server is reachable only via tcp
+if which pactl > /dev/null \
+        && (! pactl info > /dev/null 2>&1 || pactl info | grep -q 'Default Sink: auto_null' ) \
+        && env PULSE_SERVER=tcp:localhost pactl info > /dev/null 2>&1; then
+    export PULSE_SERVER=tcp:localhost
+fi
