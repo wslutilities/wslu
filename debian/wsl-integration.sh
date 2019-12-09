83bbb5fa1f98414503b13eaeb132456d3980fab3
Balint Reczey <balint.reczey@canonical.com>
2019-12-09 22:23:45 +0100

    debian/wsl-integration.sh: Set timeouts for X and PulseAudio detection
    This fixes long startup time when detection fails after waiting for long.
    
    LP: #1855520

--- debian/wsl-integration.sh
+++ debian/wsl-integration.sh
@@ -2,21 +2,29 @@ if type pactl > /dev/null 2>&1 || type xvinfo > /dev/null 2>&1; then
     # detect WSL host
     if type systemd-detect-virt > /dev/null 2>&1 && test "$(systemd-detect-virt -c)" != wsl -a -e /etc/resolv.conf; then
         WSL_HOST=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null)
+        WSL_HOST_X_TIMEOUT=0.2
+        WSL_HOST_PA_TIMEOUT=0.3
+    else
+        WSL_HOST=${WSL_HOST:-localhost}
+        WSL_HOST_X_TIMEOUT=0.6
+        WSL_HOST_PA_TIMEOUT=0.8
     fi
-    WSL_HOST=${WSL_HOST:-localhost}
+
 
     # set DISPLAY if there is an X11 server running
-    if type xvinfo > /dev/null 2>&1 && env DISPLAY=${WSL_HOST}:0 xvinfo > /dev/null 2>&1; then
+    if type xvinfo > /dev/null 2>&1 && env DISPLAY=${WSL_HOST}:0 timeout $WSL_HOST_X_TIMEOUT xvinfo > /dev/null 2>&1; then
         export DISPLAY=${WSL_HOST}:0
         export LIBGL_ALWAYS_INDIRECT=1
     fi
 
     # set up audio if pulse server is reachable only via tcp
     if type pactl > /dev/null 2>&1 \
-           && (! pactl info > /dev/null 2>&1 || pactl info 2> /dev/null | grep -q 'Default Sink: auto_null' ) \
-           && env PULSE_SERVER=tcp:${WSL_HOST} pactl info > /dev/null 2>&1; then
+           && (! timeout $WSL_HOST_PA_TIMEOUT pactl info > /dev/null 2>&1 || timeout $WSL_HOST_PA_TIMEOUT pactl info 2> /dev/null | grep -q 'Default Sink: auto_null' ) \
+           && env PULSE_SERVER=tcp:${WSL_HOST} timeout $WSL_HOST_PA_TIMEOUT pactl info > /dev/null 2>&1; then
         export PULSE_SERVER=tcp:${WSL_HOST}
     fi
 
     unset WSL_HOST
+    unset WSL_HOST_X_TIMEOUT
+    unset WSL_HOST_PA_TIMEOUT
 fi
