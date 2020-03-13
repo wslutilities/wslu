9cf6fe992645ee15546676f3cd6f283f56047458
Patrick Wu <wotingwu@live.com>
2020-03-13 16:00:14 +0800

    wslact: fixes
    - Smart mount to automount
    - Lazy load drive to improve speed

--- src/wslact.sh
+++ src/wslact.sh
@@ -28,8 +28,10 @@ function time_sync {
 	fi
 }
 
-function smart_mount {
-	local help_short="wslact smart-mount [-h]"
+function auto_mount {
+	local help_short="wslact auto-mount [-h]"
+	local mntpt_prefix="$(interop_prefix)"
+	local sysdrv_prefix="$(sysdrive_prefix)"
 
 	while [ "$1" != "" ]; do
 		case "$1" in
@@ -39,12 +41,12 @@ function smart_mount {
 	done
 
 	if [ "$EUID" -ne 0 ]
-		then echo "${error} \`wslact smart-mount\` requires you to run as root. Aborted."
+		then echo "${error} \`wslact auto-mount\` requires you to run as root. Aborted."
 		exit 1
 	fi
 
 	mount_opt=""
-	drive_list="$("$(interop_prefix)/$(sysdrive_prefix)"/WINDOWS/system32/fsutil.exe fsinfo drives | tail -1 | tr '[:upper:]' '[:lower:]' | tr -d ':\\' | sed -e 's/drives //g' -e 's|'$(sysdrive_prefix)' ||g' -e 's|\r||g' -e 's| $||g' -e 's| |\n|g')"
+	drive_list="$("$mntpt_prefix$sysdrv_prefix"/WINDOWS/system32/fsutil.exe fsinfo drives | tail -1 | tr '[:upper:]' '[:lower:]' | tr -d ':\\' | sed -e 's/drives //g' -e 's|'$sysdrv_prefix' ||g' -e 's|\r||g' -e 's| $||g' -e 's| |\n|g')"
 
 	if [ -f /etc/wsl.conf ]; then
 		tmp="$(grep ^options /etc/wsl.conf | sed -r -e 's|^options[ ]+=[ ]+||g' -e 's|^"||g' -e 's|"$||g')"
@@ -60,27 +62,27 @@ function smart_mount {
 	mount_j=0
 	
 	for drive in $drive_list; do
-		[[ -d "/mnt/$drive" ]] || mkdir -p "/mnt/$drive"
-		if [[ -n $(find "/mnt/$drive" -maxdepth 0 -type d -empty 2>/dev/null) ]]; then
-			echo "${info} Mounting Drive ${drive^} to /mnt/$drive..."
-			if mount -t drvfs ${drive}: "/mnt/$drive" -o "$mount_opt" 2>/dev/null; then
-				echo "${info} Mounted Drive ${drive^} to /mnt/$drive."
+		[[ -d "$mntpt_prefix$drive" ]] || mkdir -p "$mntpt_prefix$drive"
+		if [[ -n $(find "$mntpt_prefix$drive" -maxdepth 0 -type d -empty 2>/dev/null) ]]; then
+			echo "${info} Mounting Drive ${drive^} to $mntpt_prefix$drive..."
+			if mount -t drvfs ${drive}: "$mntpt_prefix$drive" -o "$mount_opt" 2>/dev/null; then
+				echo "${info} Mounted Drive ${drive^} to $mntpt_prefix$drive."
 				mount_s=$((mount_s + 1))
 			else
 				echo "${error} Failed to mount Drive ${drive^}. Skipped."
 				mount_f=$((mount_f + 1))
 			fi
 		else
-			echo "${warn} Already mounted Drive ${drive^} at /mnt/$drive. Skipped."
+			echo "${warn} Already mounted Drive ${drive^} at $mntpt_prefix$drive. Skipped."
 			mount_j=$((mount_j + 1))
 		fi
 	done
-	echo "${info} Smart mounting completed. $mount_s drive(s) succeed. $mount_f drive(s) failed. $mount_j drive(s) skipped."
+	echo "${info} Auto mounting completed. $mount_s drive(s) succeed. $mount_f drive(s) failed. $mount_j drive(s) skipped."
 }
 
 while [ "$1" != "" ]; do
 	case "$1" in
-		sm|smart-mount) smart_mount "$@"; exit;;
+		am|auto-mount) auto_mount "$@"; exit;;
 		ts|time-sync) time_sync "$@"; exit;;
 		-h|--help) help "$0" "$help_short"; exit;;
 		-v|--version) echo "wslu v$wslu_version; wslact v$version"; exit;;
