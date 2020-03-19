5f8b7e911f82edd10d11551925b08e41f1295ddb
Jinming Wu, Patrick <me@patrickwu.space>
2020-03-19 22:59:00 +0800

    ci: fix file not found issue

--- configure.sh
+++ configure.sh
@@ -67,6 +67,19 @@ make
 sudo make DESTDIR=/usr install
 }
 
+function deb_build_prep {
+	BUILD_VER_NUM=$(cat ./VERSION | cut -f1 -d-)
+	REL_VER_NUM=$(cat ./VERSION | cut -f2 -d-)
+	sed -i s/VERSIONPLACEHOLDER/"$(cat ./VERSION)"/g ./src/wslu-header
+	sed -i s/BUILDVERPLACEHOLDER/"$BUILD_VER_NUM"/g ./extras/build/rpm/wslu.spec
+	sed -i s/RELVERPLACEHOLDER/"$REL_VER_NUM"/g ./extras/build/rpm/wslu.spec
+	mkdir -p ../wslu-$BUILD_VER_NUM/
+	cp -r * ../wslu-$BUILD_VER_NUM/
+	cd ../wslu-$BUILD_VER_NUM
+	tar -czvf wslu-$BUILD_VER_NUM.tar.gz *
+	cp wslu-$BUILD_VER_NUM.tar.gz ../wslu
+	cd ../wslu
+}
 
 function rpm_build_prep {
 	BUILD_VER_NUM=$(cat ./VERSION | cut -f1 -d-)
