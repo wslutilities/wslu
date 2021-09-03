ae558191080ff2167d57dd1a9e3626270049e92c
Isaac <IsaacOscar@live.com.au>
2021-09-03 13:16:14 +1200

    make wslview always use wslpath

--- src/wslview.sh
+++ src/wslview.sh
@@ -42,28 +42,16 @@ if [[ "$lname" != "" ]]; then
 	if [[ "$lname" =~ ^file:\/\/.*$ ]] && [[ ! "$lname" =~ ^file:\/\/(\/)+[A-Za-z]\:.*$ ]]; then
 		[ $wslutmpbuild -ge "$BN_MAY_NINETEEN" ] || error_echo "This protocol is not supported before version 1903." 34
 		properfile_full_path="$(readlink -f "${lname//file:\/\//}")"
-		interop_win_type="$(interop_prefix)$(sysdrive_prefix)"
-		converted_file_path="\\\\wsl\$\\$WSL_DISTRO_NAME${properfile_full_path//\//\\}"
-		[[ "$properfile_full_path" =~ ^${interop_win_type//\/$/}.*$ ]] && converted_file_path="$(wslpath -w "$properfile_full_path")"
-		lname="$converted_file_path"
 	# Linux absolute path
 	elif [[ "$lname" =~ ^(/[^/]+)*(/)?$ ]]; then
 		[ $wslutmpbuild -ge "$BN_MAY_NINETEEN" ] || error_echo "This protocol is not supported before version 1903." 34
 		properfile_full_path="$(readlink -f "${lname}")"
-		interop_win_type="$(interop_prefix)$(sysdrive_prefix)"
-		converted_file_path="\\\\wsl\$\\$WSL_DISTRO_NAME${properfile_full_path//\//\\}"
-		[[ "$properfile_full_path" =~ ^${interop_win_type//\/$/}.*$ ]] && converted_file_path="$(wslpath -w "$properfile_full_path")"
-		lname="$converted_file_path"
 	# Linux relative path
 	elif [[ -d "$(readlink -f "$lname")" ]] || [[ -f "$(readlink -f "$lname")" ]]; then
 		[ $wslutmpbuild -ge "$BN_MAY_NINETEEN" ] || error_echo "This protocol is not supported before version 1903." 34
 		properfile_full_path="$(readlink -f "${lname}")"
-		interop_win_type="$(interop_prefix)$(sysdrive_prefix)"
-		converted_file_path="\\\\wsl\$\\$WSL_DISTRO_NAME${properfile_full_path//\//\\}"
-		[[ "$properfile_full_path" =~ ^${interop_win_type//\/$/}.*$ ]] && converted_file_path="$(wslpath -w "$properfile_full_path")"
-		lname="$converted_file_path"
 	fi
-	winps_exec Start "\"$lname\""
+	winps_exec Start "\"$(wslpath -w "$properfile_full_path")\""
 else
 	error_echo "No input, aborting" 21
 fi
